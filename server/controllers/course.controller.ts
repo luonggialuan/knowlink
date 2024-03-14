a244eb401b836b31b9cd380560a5c4a7c9a665f9
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-14 22:41:37 +0700

    Edit a course functionality

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { CatchAsyncError } from '../middleware/catchAsyncErrors'
 import ErrorHandler from '../utils/ErrorHandler'
 import cloudinary from 'cloudinary'
 import { createCourse } from '../services/course.service'
+import courseModel from '../models/course.model'
 
 // Upload course
 export const uploadCourse = CatchAsyncError(
@@ -22,6 +23,48 @@ export const uploadCourse = CatchAsyncError(
         }
       }
       createCourse(data, res, next)
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400))
+    }
+  }
+)
+
+//  Edit course
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body
+      const thumbnail = data.thumbnail
+
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: 'courses'
+        })
+
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url
+        }
+      }
+
+      const courseId = req.params.id
+
+      const course = await courseModel.findByIdAndUpdate(
+        courseId,
+        {
+          $set: data
+        },
+        {
+          new: true
+        }
+      )
+
+      res.status(201).json({
+        success: true,
+        course
+      })
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400))
     }
