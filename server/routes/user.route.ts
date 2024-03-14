d1fc2228e2cc6ac718d8075a63376e674ab5b2b5
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-14 11:15:40 +0700

    Get user information and user social authentication

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import {
   activateUser,
+  getUserInfo,
   loginUser,
   logoutUser,
   registrationUser,
+  socialAuth,
   updateAccessToken
 } from '../controllers/user.controller'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
@@ -15,5 +17,7 @@ userRouter.post('/activate-user', activateUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/logout', isAuthenticated, logoutUser)
 userRouter.get('/refresh', updateAccessToken)
+userRouter.get('/me', isAuthenticated, getUserInfo)
+userRouter.get('/social-auth', socialAuth)
 
 export default userRouter
