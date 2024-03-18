94034c2ec7201fe747e7c7f1c373a6849645d9a2
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-18 17:57:56 +0700

    Get notifications for admin

---
+++ server/controllers/notification.controller.ts
@@ -0,0 +1,22 @@
+import NotificationModel from '../models/notification.model'
+import { NextFunction, Request, Response } from 'express'
+import { CatchAsyncError } from '../middleware/catchAsyncErrors'
+import ErrorHandler from '../utils/ErrorHandler'
+
+// Get all notifications - Only for admin
+export const getNotifications = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1
+      })
+
+      res.status(201).json({
+        success: true,
+        notifications
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500))
+    }
+  }
+)
