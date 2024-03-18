9e50882039d7c0ef816a17be1bc5cd7e0a018682
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-18 21:25:08 +0700

    Delete read notifications after 30 days

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import NotificationModel from '../models/notification.model'
 import { NextFunction, Request, Response } from 'express'
 import { CatchAsyncError } from '../middleware/catchAsyncErrors'
 import ErrorHandler from '../utils/ErrorHandler'
+import cron from 'node-cron'
 
 // Get all notifications - Only for admin
 export const getNotifications = CatchAsyncError(
@@ -50,3 +51,12 @@ export const updateNotification = CatchAsyncError(
     }
   }
 )
+
+// Delete notification - Only for admin
+cron.schedule('0 0 0 * * *', async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+  await NotificationModel.deleteMany({
+    status: 'read',
+    createdAt: { $lt: thirtyDaysAgo }
+  })
+})
