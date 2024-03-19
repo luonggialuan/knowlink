aefbed9df515e6f77ddae6042a240f2a10757270
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-19 20:22:41 +0700

    Get the last 28 days information analytics from users, courses, and order

---
+++ server/controllers/analytics.controller.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express'
+import { CatchAsyncError } from '../middleware/catchAsyncErrors'
+import ErrorHandler from '../utils/ErrorHandler'
+import { generateLast12MonthsData } from '../utils/analytics.generator'
+import UserModel from '../models/user.model'
+import CourseModel from '../models/course.model'
+import OrderModel from '../models/order.model'
+
+// Get users analytics - Only for admin
+export const getUsersAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await generateLast12MonthsData(UserModel)
+
+      res.status(200).json({
+        success: true,
+        users
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500))
+    }
+  }
+)
+
+// Get courses analytics - Only for admin
+export const getCoursesAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await generateLast12MonthsData(CourseModel)
+
+      res.status(200).json({
+        success: true,
+        courses
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500))
+    }
+  }
+)
+
+// Get orders analytics - Only for admin
+export const getOrdersAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const orders = await generateLast12MonthsData(OrderModel)
+
+      res.status(200).json({
+        success: true,
+        orders
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500))
+    }
+  }
+)
